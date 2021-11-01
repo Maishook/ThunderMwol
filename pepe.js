f3db6b1b09aa5c08953439dc269224e0645d6cb1
pepesir <90826638+pepesir@users.noreply.github.com>
2021-11-01 10:05:36 +0530

    Create pepe.js

---
+++ pepe.js
@@ -0,0 +1,18 @@
+/* codded by pepe sir
+*/
+
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+// PLK Special Functions
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+
+ Z_NUMBER: process.env.NUMBER === undefined ? '+917736622139' : process.env.NUMBER,
+ Z_OWNER: process.env.OWNER_NAME === undefined ? 'pepesir' : process.env.OWNER_NAME,
+
+};
