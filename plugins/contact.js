45be694a265084d2a5a2a91f298a7b58f6314aea
pepesir <90826638+pepesir@users.noreply.github.com>
2021-11-01 22:22:56 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -4,9 +4,9 @@ const exec = require('child_process').exec;
 const os = require("os");
 const fs = require('fs');
 const Config = require('../config')
-Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
+Ktb.addCommand({ pattern: 'OWNER ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
 
-//coded by saidali
+//coded by PEPE
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
             + 'FN:Amalser [OWNER]\n' // full name
